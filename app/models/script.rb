ff7c1e78fafd44794683a284266f8b7ad36aaa9c
Cameron <Alleyria@gmail.com>
2022-02-15 13:34:36 +0100

    script and serve

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,2 +1,11 @@
+require "stringio"
+
 class Script < ApplicationRecord
+  has_one_attached :doc
+
+  after_create :build_doc
+
+  def build_doc
+    doc.attach io: StringIO.new("Hello World"), filename: "testdoc"
+  end
 end
