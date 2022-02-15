c6f33fe95a1b00f5f7dd9200c7735db5fc71c3dd
Cameron <Alleyria@gmail.com>
2022-02-15 15:58:03 +0100

    wip

--- app/models/script.rb
+++ app/models/script.rb
@@ -3,7 +3,7 @@
 class Script < ApplicationRecord
   has_one_attached :doc
 
-  after_commit :build_doc
+  after_create :build_doc
 
   def build_doc
     doc.attach io: DocumentBuilder.new(text).call, filename: "testdoc"
