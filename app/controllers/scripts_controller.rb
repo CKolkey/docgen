bd7725c5ff900c58dac73d6374d2401a959a1958
Cameron <Alleyria@gmail.com>
2022-02-16 14:01:24 +0100

    wip

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -10,12 +10,10 @@ class ScriptsController < ApplicationController
   def update
     @script = Script.first
 
-    ActiveRecord::Base.transaction do
-      @script.update(script_params)
-      @script.build_doc
-    end
+    @script.update(script_params)
+    @script.build_doc
 
-    ActiveRecord::Base.connection.execute("BEGIN TRANSACTION; END;")
+    ActiveRecord::Base.connection.execute('BEGIN TRANSACTION; END;')
     redirect_to scripts_path
   end
 
