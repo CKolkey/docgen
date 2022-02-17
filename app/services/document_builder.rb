87d38ae6d79798a51512e97d055df96c81d2fd6f
Cameron <Alleyria@gmail.com>
2022-02-17 11:55:24 +0100

    wip

--- app/services/document_builder.rb
+++ app/services/document_builder.rb
@@ -7,7 +7,7 @@ class DocumentBuilder
 
   def call
     Dir.mktmpdir do |dir|
-      File.open(File.join(dir, 'directive'), 'w+') { |f| f.write template(File.join(dir, 'presentation')) }
+      File.open(File.join(dir, 'directive'), 'w+') { |f| f.write template(dir, "presentation") }
       copy_images(dir)
 
       status = `documentbuilder #{File.join(dir, 'directive')}  2>&1`
@@ -21,11 +21,12 @@ class DocumentBuilder
     FileUtils.cp_r(Rails.root.join("app", "assets", "images"), dest)
   end
 
-  def template(filename)
+  def template(dir, filename)
     <<~TEMPLATE
       builder.CreateFile("pptx");
+      tmpDir = "#{dir}/";
       #{@directive}
-      builder.SaveFile("pptx", "#{filename}.pptx");
+      builder.SaveFile("pptx", "#{File.join(dir, filename)}.pptx");
       builder.CloseFile();
     TEMPLATE
   end
